962a464b3a8da2006a20f55658d3a3904d15d060
Knursky <47036541+Knursky@users.noreply.github.com>
2021-05-16 15:09:33 +0200

    task14

--- src/wsb/database/ObjectToSql.java
+++ src/wsb/database/ObjectToSql.java
@@ -1,19 +1,21 @@
 package wsb.database;
-import wsb.annotations.Mapped;
+
+import wsb.annotations.MappedClass;
 import wsb.creatures.Human;
 import wsb.creatures.Pet;
 import wsb.food.FoodType;
 
 import java.lang.reflect.Field;
-import java.sql.SQLException;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ObjectToSql {
     public String insert(Object object) throws IllegalAccessException {
+        if (!object.getClass().isAnnotationPresent(MappedClass.class))
+            return null;
         List<Field> fields = Arrays.stream(object.getClass().getFields())
-                .filter(field -> field.isAnnotationPresent(Mapped.class))
                 .collect(Collectors.toList());
         String name = object.getClass().getName();
         Object[] fieldValues = new Object[fields.size()];
