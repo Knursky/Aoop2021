7bf9781e8d337e27e8c06cfebfc097bc40b64c6c
Knursky <47036541+Knursky@users.noreply.github.com>
2021-05-16 12:39:55 +0200

    task13

--- src/wsb/database/ObjectToSql.java
+++ src/wsb/database/ObjectToSql.java
@@ -1,5 +1,5 @@
 package wsb.database;
-
+import wsb.annotations.Mapped;
 import wsb.creatures.Human;
 import wsb.creatures.Pet;
 import wsb.food.FoodType;
@@ -7,23 +7,29 @@ import wsb.food.FoodType;
 import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ObjectToSql {
     public String insert(Object object) throws IllegalAccessException {
-        Field[] fields = object.getClass().getFields();
+        List<Field> fields = Arrays.stream(object.getClass().getFields())
+                .filter(field -> field.isAnnotationPresent(Mapped.class))
+                .collect(Collectors.toList());
         String name = object.getClass().getName();
-        Object[] fieldValues = new Object[fields.length];
-        for (int i = 0; i < fields.length; i++) {
-            fields[i].setAccessible(true);
-            fieldValues[i] = fields[i].get(object);
+        Object[] fieldValues = new Object[fields.size()];
+        for (int i = 0; i < fields.size(); i++) {
+            fields.get(i).setAccessible(true);
+            fieldValues[i] = fields.get(i).get(object);
         }
-        String fieldsAsStrings = Arrays.stream(fields).map(Field::getName).collect(Collectors.joining(", "));
+        String fieldsAsStrings = fields
+                .stream()
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
         String fieldValuesAsString = Arrays.stream(fieldValues).map(o -> o == null ? "null" : o.toString()).collect(Collectors.joining(", "));
         return "INSERT INTO " + name + " ( " + fieldsAsStrings + " ) VALUES ( " + fieldValuesAsString + " )";
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         Human human = new Human(2, 2, FoodType.ALL);
         Pet pet = new Pet("Dog", FoodType.MEAT);
         human.pet = pet;
